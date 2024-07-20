f5f593516bd2aefd8535777616af4044c012f76c
manuBarrionuevo <manuelbarrionuevo678@hotmail.com>
2024-07-20 19:48:12 -0300

    Agregar ec2_user_data.sh para configuración de EC2

--- create_ec2.sh
+++ create_ec2.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-# Variables
-AMI_ID="ami-04a81a99f5ec58529"
+# Configura variables
+AMI_ID="ami-0b69ea66ff7391e80" # Ubuntu Server 22.04 en us-east-1
 INSTANCE_TYPE="t2.micro"
 KEY_NAME="pin"
-SECURITY_GROUP="allow_ssh_http"
-TAG_NAME="pin"
+SECURITY_GROUP_NAME="pin-security-group"
 
-# Crear instancia EC2
+# Crea el grupo de seguridad si no existe
+SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=$SECURITY_GROUP_NAME --query 'SecurityGroups[*].GroupId' --output text)
+if [ -z "$SECURITY_GROUP_ID" ]; then
+  SECURITY_GROUP_ID=$(aws ec2 create-security-group --group-name $SECURITY_GROUP_NAME --description "Security group for PIN-FINAL project" --output text)
+  aws ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --protocol tcp --port 22 --cidr 0.0.0.0/0
+  aws ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --protocol tcp --port 80 --cidr 0.0.0.0/0
+fi
+
+# Ejecuta la instancia
 aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type $INSTANCE_TYPE \
-    --key-name $KEY_NAME \
-    --security-groups $SECURITY_GROUP \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$TAG_NAME'}]' \
-    --user-data file://ec2_user_data.sh
+  --image-id $AMI_ID \
+  --count 1 \
+  --instance-type $INSTANCE_TYPE \
+  --key-name $KEY_NAME \
+  --security-group-ids $SECURITY_GROUP_ID \
+  --user-data file://ec2_user_data.sh \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=PIN-FINAL-Instance}]'
+
+# Espera a que la instancia se ejecute
+INSTANCE_ID=$(aws ec2 describe-instances --filters Name=tag:Name,Values=PIN-FINAL-Instance --query 'Reservations[*].Instances[*].InstanceId' --output text)
+aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+
+# Imprime la IP pública de la instancia
+PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+echo "La instancia EC2 se ha iniciado con la IP pública: $PUBLIC_IP"
