7491426a0c67260f2e481328b99052c1fcd2e9ba
manuBarrionuevo <manuelbarrionuevo678@hotmail.com>
2024-07-21 20:23:06 -0300

    update pryecto

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,106 @@
-# VPC
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+
   tags = {
     Name = "main-vpc"
   }
 }
 
-# Subnet
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_cidr
+resource "aws_subnet" "subnet_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_a_cidr
+  availability_zone = "us-east-1a"
+
   tags = {
-    Name = "main-subnet"
+    Name = "subnet-a"
   }
 }
 
-# Security Group
+resource "aws_subnet" "subnet_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_b_cidr
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "subnet-b"
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sg"
+  }
+}
+
 resource "aws_security_group" "eks_sg" {
-  vpc_id = aws_vpc.main.id
+  description = "Managed by Terraform"
+  vpc_id      = aws_vpc.main.id
+
   tags = {
     Name = "eks-sg"
   }
 }
 
-# EKS Cluster
+resource "aws_ebs_volume" "ebs_volume_grafana" {
+  availability_zone = "us-east-1a"
+  size              = 10
+
+  tags = {
+    Name = "ebs-volume-grafana"
+  }
+}
+
+resource "aws_ebs_volume" "ebs_volume_prometheus" {
+  availability_zone = "us-east-1a"
+  size              = 10
+
+  tags = {
+    Name = "ebs-volume-prometheus"
+  }
+}
+
+resource "aws_iam_role" "eks_role" {
+  name               = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
 resource "aws_eks_cluster" "main" {
-  name     = var.cluster_name
+  name     = var.eks_cluster_name
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.main.id]
+    subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
   }
 
   depends_on = [
@@ -37,53 +108,17 @@ resource "aws_eks_cluster" "main" {
   ]
 }
 
-resource "aws_iam_role" "eks_role" {
-  name = "eks-cluster-role"
+resource "aws_instance" "kubectl_instance" {
+  ami           = "ami-0de53d8956e8dcf80" # AMI válida para us-east-1
+  instance_type = var.instance_type
+  key_name      = var.key_pair_name
+  subnet_id     = aws_subnet.subnet_a.id
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
+  security_groups = [aws_security_group.ec2_sg.id]
 
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_role.name
-}
-
-# EBS Volumes
-resource "aws_ebs_volume" "ebs_volume_prometheus" {
-  availability_zone = "us-east-1a"
-  size              = var.ebs_volume_size
   tags = {
-    Name = "ebs-volume-prometheus"
+    Name = "kubectl-instance"
   }
-}
 
-resource "aws_ebs_volume" "ebs_volume_grafana" {
-  availability_zone = "us-east-1a"
-  size              = var.ebs_volume_size
-  tags = {
-    Name = "ebs-volume-grafana"
-  }
-}
-
-output "ebs_volume_prometheus_id" {
-  value = aws_ebs_volume.ebs_volume_prometheus.id
-}
-
-output "ebs_volume_grafana_id" {
-  value = aws_ebs_volume.ebs_volume_grafana.id
-}
-
-output "cluster_name" {
-  value = aws_eks_cluster.main.name
+  user_data = file("user_data.sh")
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,25 +1,41 @@
+variable "aws_region" {
+  description = "The AWS region to deploy in"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "The CIDR block for the VPC"
+  type        = string
   default     = "10.0.0.0/16"
 }
 
-variable "subnet_cidr" {
-  description = "CIDR block for the subnet"
+variable "subnet_a_cidr" {
+  description = "The CIDR block for subnet A"
+  type        = string
   default     = "10.0.1.0/24"
 }
 
-variable "cluster_name" {
-  description = "EKS Cluster Name"
-  default     = "my-cluster"
+variable "subnet_b_cidr" {
+  description = "The CIDR block for subnet B"
+  type        = string
+  default     = "10.0.2.0/24"
 }
 
-variable "ebs_volume_size" {
-  description = "Size of the EBS volume in GB"
-  default     = 10
+variable "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+  default     = "DevOpsGroup7"
 }
 
-variable "aws_account_id" {
-  description = "The AWS account ID"
-  default     = "471112727282"
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+  default     = "t2.micro"
 }
 
+variable "key_pair_name" {
+  description = "The name of the key pair to use for the EC2 instance"
+  type        = string
+  default     = "eks-key-pair"
+}
